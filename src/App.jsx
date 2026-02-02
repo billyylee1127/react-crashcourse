d1157305e9f918ac813ce6158f7a80118f40257c
Billy Lee <billylee@billys-air.mynetworksettings.com>
2026-02-02 13:44:42 -0600

    error after commenting out Modal

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import Todo from "./components/Todo";
+import Title from "./components/Title";
+import Modal from "./components/Modal.jsx";
+
+function App() {
+  return (
+    <div>
+      <Title />
+      <div className="todo__wrapper">
+        <Todo
+          title="Finish Frontend Simplified"
+          paragraph="Code along with Frontend Simplified step by step."
+        />
+        <Todo
+          title="Finish Interview Section"
+          paragraph="Finish every interview question in the nest 6 weeks."
+        />
+        <Todo title="Land a $100k job" paragraph="Apply to 100 jobs" />
+      </div>
+      {/* <Modal title="Are you sure you want to delete?" /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,16 @@
+import "./Todo.css"
+
+function Todo({ title, paragraph }) {
+    console.log(title)
+    return (
+    <div className="todo">
+        <h2>{title}</h2>
+        <p>{pargraph}</p>
+        <button onClick={() => {
+            console.log=`clicked`
+        }}>Delete</button>
+    </div>
+    );
+}
+
+export default Todo;

a556bd597ab208927a3300a55e5f3b419de89a48
Billy Lee <billylee@billys-air.mynetworksettings.com>
2026-02-13 20:09:49 -0600

    no user id showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from `react-router-dom`
-import Home from `./pages/Home.jsx`
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
+import Contact from "./pages/Contact.jsx"
+import Nav from "./components/Nav.jsx";
+import Users from "./pages/Users"
+
 function App() {
   
 
   return (
     <div>
       <Router>
+        <Nav />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<Users />} />
         </Routes>
       </Router>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,40 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import User from "../components/User.jsx";
+import { Link } from "react-router-dom"
+
 function Home() {
-    return <h1>Home</h1>
+  const [users, setUsers] = useState([]);
+
+  async function fetchUsers() {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users",
+    );
+    setUsers(data);
+    console.log(data);
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchUsers();
+    }, 500);
+  }, []);
+  const pixels = "3px";
+
+  return (
+    <div>
+      {users.map((user) => (
+        <Link to={`/${user.id}`} key={user.id}>
+          <User            
+            id={user.id}
+            name={user.name}
+            email={user.emai}
+            username={user.username}
+          />
+        </Link>
+      ))}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
